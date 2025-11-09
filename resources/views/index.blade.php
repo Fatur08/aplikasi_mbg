15e23118e27ed083668cd1ad896b99c7177bc58a
Fatur08 <dhutafaturrahman@gmail.com>
2025-11-09 09:25:55 +0700

    edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -70,7 +70,7 @@
 
 <div class="login-container">
     <div class="login-title">
-        Sistem Program MBG<br>Kabupaten Lampung Timur
+        Sistem Program MBG<br>Kabupaten Lampung Timur dan Selatan
     </div>
 
     <img src="{{ asset('assets/img/logo_SPPG_lampung_timur(200x200).png') }}" 
