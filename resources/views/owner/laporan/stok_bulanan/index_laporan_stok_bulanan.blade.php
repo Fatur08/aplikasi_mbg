46ecfb0b5022bc7209dcf7bcfcde30abaea1f0b5
Fatur08 <dhutafaturrahman@gmail.com>
2025-12-03 23:34:10 +0700

    update

--- resources/views/owner/laporan/stok_bulanan/index_laporan_stok_bulanan.blade.php
+++ resources/views/owner/laporan/stok_bulanan/index_laporan_stok_bulanan.blade.php
@@ -272,37 +272,39 @@
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    @php $no = 1; @endphp
-                                                    @foreach($data_laporan as $item)
-                                                                                        
-                                                        @php
-                                                            switch($item['keterangan']){
-                                                                case 'Aman':
-                                                                    $badge = 'bg-success';
-                                                                    break;
-                                                                case 'Menipis':
-                                                                    $badge = 'bg-warning';
-                                                                    break;
-                                                                case 'Habis':
-                                                                    $badge = 'bg-danger';
-                                                                    break;
-                                                                default:
-                                                                    $badge = 'bg-dark';
-                                                            }
-                                                        @endphp
-                                                        
-                                                    <tr>
-                                                        <td class="text-center">{{ $no++ }}</td>
-                                                        <td>{{ $item['nama_bahan'] }}</td>
-                                                        <td>{{ $item['satuan'] }}</td>
-                                                        <td class="text-center">{{ $item['total_masuk'] }}</td>
-                                                        <td class="text-center">{{ $item['total_keluar'] }}</td>
-                                                        <td class="text-center">{{ $item['stok_akhir'] }}</td>
-                                                        <td class="text-center">
-                                                            <span class="badge {{ $badge }}">{{ $item['keterangan'] }}</span>
-                                                        </td>
-                                                    </tr>
-                                                    @endforeach
+                                                    @if(empty($data_laporan) || count($data_laporan) == 0)
+                                                        <tr>
+                                                            <td colspan="8" style="text-align:center; color:red;">
+                                                                Tidak ada data stok pada bulan ini
+                                                            </td>
+                                                        </tr>
+                                                    @else
+                                                        @php $no = 1; @endphp
+                                                        @foreach($data_laporan as $item)
+
+                                                            @php
+                                                                switch($item['keterangan']){
+                                                                    case 'Aman': $badge = 'bg-success'; break;
+                                                                    case 'Menipis': $badge = 'bg-warning'; break;
+                                                                    case 'Habis': $badge = 'bg-danger'; break;
+                                                                    default: $badge = 'bg-dark';
+                                                                }
+                                                            @endphp
+                                                            
+                                                            <tr>
+                                                                <td class="text-center">{{ $no++ }}</td>
+                                                                <td>{{ $item['nama_bahan'] }}</td>
+                                                                <td>{{ $item['satuan'] }}</td>
+                                                                <td class="text-center">{{ $item['total_masuk'] }}</td>
+                                                                <td class="text-center">{{ $item['total_keluar'] }}</td>
+                                                                <td class="text-center">{{ $item['stok_akhir'] }}</td>
+                                                                <td class="text-center">
+                                                                    <span class="badge {{ $badge }}">{{ $item['keterangan'] }}</span>
+                                                                </td>
+                                                            </tr>
+                                                            
+                                                        @endforeach
+                                                    @endif             
                                                 </tbody>
                                             </table>
                                         </div>
