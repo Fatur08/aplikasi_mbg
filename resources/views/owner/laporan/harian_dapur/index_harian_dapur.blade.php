f219bf5384108258cf174d8084f5846bf3e176ce
Fatur08 <dhutafaturrahman@gmail.com>
2025-12-04 10:20:01 +0700

    update

--- resources/views/owner/laporan/harian_dapur/index_harian_dapur.blade.php
+++ resources/views/owner/laporan/harian_dapur/index_harian_dapur.blade.php
@@ -303,15 +303,27 @@
                                                                 </a>
                                                             </div>
                                                         </td>
-                                                        <td>
-                                                            @if($item->status_jadwal_menu_harian == 1)
-                                                                <span class="badge bg-success">Selesai</span>
+                                                        <td style="text-align: center; width:10%">
+                                                            @if ($item->status_jadwal_menu_harian == 2)
+                                                                <span class="badge bg-success">Diterima</span>
+                                                            @elseif ($item->status_jadwal_menu_harian == 1)
+                                                                <span class="badge bg-warning text-dark">Sekarang</span>
+                                                            @elseif ($item->status_jadwal_menu_harian == 3)
+                                                                <span class="badge bg-danger text-dark">Tersisa</span>
                                                             @else
-                                                                <span class="badge bg-warning">Belum</span>
+                                                                <span class="badge bg-secondary">Besok</span>
                                                             @endif
                                                         </td>
-                                                        <td>
-                                                            <a href="#" class="btn btn-sm btn-info">Detail</a>
+                                                        <td style="text-align: center; width:10%">
+                                                            <div class="btn-group">
+                                                                <form action="/kepala_dapur/menu_harian/{{ $item->id_jadwal_menu_harian }}/delete_jadwal_menu_harian" style="margin-left: 5px;" method="POST">
+                                                                    @csrf
+                                                                    <a class="btn btn-danger btn-sm delete-confirm-menuharian" >
+                                                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16z" /><path d="M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z" /></svg>
+                                                                        Hapus
+                                                                    </a>
+                                                                </form>
+                                                            </div>
                                                         </td>
                                                     </tr>
                                                 @empty
