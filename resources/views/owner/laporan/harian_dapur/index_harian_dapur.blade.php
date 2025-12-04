299a4990a1c3c1542f417b5362d2e6daaa3ad4bb
Fatur08 <dhutafaturrahman@gmail.com>
2025-12-04 10:14:55 +0700

    update

--- resources/views/owner/laporan/harian_dapur/index_harian_dapur.blade.php
+++ resources/views/owner/laporan/harian_dapur/index_harian_dapur.blade.php
@@ -273,10 +273,6 @@
                                                         <td>{{ $item->jumlah_porsi_menu_harian }}</td>
                                                         <td style="text-align: center; width:25%"> 
                                                             <div class="align-items-center">
-                                                                <!--<a href="#" class="tambah_bahan_terpakai btn btn-info btn-sm" data-id="{{ $menu->id_jadwal_menu_harian }}">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
-                                                                    <span>Tambah</span>
-                                                                </a>-->
                                                                 <a href="#" class="lihat_bahan_terpakai btn btn-info btn-sm" data-id="{{ $menu->id_jadwal_menu_harian }}">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -293,10 +289,6 @@
                                                         </td>
                                                         <td style="text-align: center; width:25%"> 
                                                             <div class="align-items-center">
-                                                                <!--<a href="#" class="tambah_kendala btn btn-info btn-sm" data-id="{{ $menu->id_jadwal_menu_harian }}">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
-                                                                    <span>Tambah</span>
-                                                                </a>-->
                                                                 <a href="#" class="lihat_kendala btn btn-info btn-sm" data-id="{{ $menu->id_jadwal_menu_harian }}">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
