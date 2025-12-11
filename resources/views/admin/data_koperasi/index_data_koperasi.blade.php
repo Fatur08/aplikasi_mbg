deba4eb7da09e0f6c62b27af6c5e782929640a17
Fatur08 <dhutafaturrahman@gmail.com>
2025-12-11 14:30:38 +0700

    update

--- resources/views/admin/data_koperasi/index_data_koperasi.blade.php
+++ resources/views/admin/data_koperasi/index_data_koperasi.blade.php
@@ -74,7 +74,7 @@
                                                 <input type="text" value="" id="sampai_tanggal" name="sampai_tanggal" class="form-control" placeholder="Sampai Tanggal" autocomplete="off">
                                             </div>
                                         </div>-->
-                                        <div class="col-md-5">
+                                        <div class="col-md-4">
                                             <div class="input-icon">
                                                 <select name="bulan" id="bulan" class="form-select">
                                                     <option value="">Bulan</option>
@@ -93,7 +93,7 @@
                                                 </select>
                                             </div>
                                         </div>
-                                        <div class="col-md-5">
+                                        <div class="col-md-4">
                                             <div class="form-group">
                                                 <button type="submit" class="btn btn-primary w-100">
                                                     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
@@ -102,7 +102,7 @@
                                             </div>
                                         </div>
                                 </form>
-                                        <div class="col-md-2">
+                                        <div class="col-md-4">
                                             <div class="form-group">
                                                 <a href="#" class="btn btn-success w-100" id="cetak_data_koperasi" >
                                                     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-printer"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" /><path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" /><path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" /></svg>
