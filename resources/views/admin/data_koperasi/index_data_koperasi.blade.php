4385affe9d9f6436e5566661c2c01f4753458c3d
Fatur08 <dhutafaturrahman@gmail.com>
2025-12-12 14:43:41 +0700

    update

--- resources/views/admin/data_koperasi/index_data_koperasi.blade.php
+++ resources/views/admin/data_koperasi/index_data_koperasi.blade.php
@@ -677,11 +677,10 @@
     });
 
     document.getElementById("cetak_data_koperasi").addEventListener("click", function() {
-        let dari = document.getElementById("dari_tanggal").value;
-        let sampai = document.getElementById("sampai_tanggal").value;
+        let dapur = document.getElementById("pilih_dapur").value;
         let bulan = document.getElementById("bulan").value;
 
-        let url = `/admin/data_koperasi/cetak_data_koperasi?dari_tanggal=${dari}&sampai_tanggal=${sampai}&bulan=${bulan}`;
+        let url = `/admin/data_koperasi/cetak_data_koperasi?dapur=${dapur}&bulan=${bulan}`;
         window.open(url, "_blank");
     });
 </script>
