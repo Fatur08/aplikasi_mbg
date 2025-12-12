60b5ee2eca7296d9b6171798b6a806386b4e36c9
Fatur08 <dhutafaturrahman@gmail.com>
2025-12-12 15:01:15 +0700

    update

--- resources/views/admin/laporan/stok_harian/index_laporan_stok_harian.blade.php
+++ resources/views/admin/laporan/stok_harian/index_laporan_stok_harian.blade.php
@@ -180,7 +180,7 @@
                                 <div class="col-12">
                                     <form action="/admin/laporan/stok_harian" method="GET" id="FormLaporanStokHarian">
                                         <div class="row">
-                                            <div class="col-md-4">
+                                            <div class="col-md-6">
                                                 <div class="input-icon">
                                                     <span class="input-icon-addon">
                                                         <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M16 3l0 4" /><path d="M8 3l0 4" /><path d="M4 11l16 0" /><path d="M8 15h2v2h-2z" /></svg>
