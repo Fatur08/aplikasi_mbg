7b4f7311e0bc56a4d24e901bc0cd36649b7e5930
Fatur08 <dhutafaturrahman@gmail.com>
2025-12-12 22:20:58 +0700

    update

--- resources/views/owner/laporan/keuangan/index_laporan_keuangan.blade.php
+++ resources/views/owner/laporan/keuangan/index_laporan_keuangan.blade.php
@@ -324,6 +324,7 @@
 </div>
 @endsection
 @push('myscript')
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
     $(function(){
         $("#tanggal_laporan_keuangan").datepicker({ 
