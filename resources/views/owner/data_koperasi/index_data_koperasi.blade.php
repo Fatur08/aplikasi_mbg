34b2297d33fa2dffcdd9a90169d921dd5d52a7fe
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-13 22:55:01 +0700

    update

--- resources/views/owner/data_koperasi/index_data_koperasi.blade.php
+++ resources/views/owner/data_koperasi/index_data_koperasi.blade.php
@@ -45,26 +45,10 @@
                             <div class="col-12">
                                 <form action="/owner/data_koperasi" method="GET" id="FormDataKoperasiOwner">
                                     <div class="row g-2 align-items-end">
-                                        <!--<div class="col-md-2">
-                                            <div class="input-icon">
-                                                <span class="input-icon-addon">
-                                                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M16 3l0 4" /><path d="M8 3l0 4" /><path d="M4 11l16 0" /><path d="M8 15h2v2h-2z" /></svg>
-                                                </span>
-                                                <input type="text" value="" id="dari_tanggal" name="dari_tanggal" class="form-control" placeholder="Dari Tanggal" autocomplete="off">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <div class="input-icon">
-                                                <span class="input-icon-addon">
-                                                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M16 3l0 4" /><path d="M8 3l0 4" /><path d="M4 11l16 0" /><path d="M8 15h2v2h-2z" /></svg>
-                                                </span>
-                                                <input type="text" value="" id="sampai_tanggal" name="sampai_tanggal" class="form-control" placeholder="Sampai Tanggal" autocomplete="off">
-                                            </div>
-                                        </div>-->
                                         <div class="col-md-4">
                                             <div class="input-icon">
                                                 <select name="pilih_dapur" id="pilih_dapur" class="form-select">
-                                                    <option value="">Dapur</option>
+                                                    <option value="">Pilih Dapur</option>
                                                     @foreach($dapurList as $dapur)
                                                         <option value="{{ $dapur->nomor_dapur }}">{{ $dapur->nama_dapur }}</option>
                                                     @endforeach
