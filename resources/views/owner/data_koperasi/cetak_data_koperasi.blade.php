b003cfad66da6159f58bca6fb341196bc423ecb4
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-14 23:30:55 +0700

    update

--- resources/views/owner/data_koperasi/cetak_data_koperasi.blade.php
+++ resources/views/owner/data_koperasi/cetak_data_koperasi.blade.php
@@ -108,6 +108,12 @@
         </h4>
     </div>
 
+    @php
+        $grouped = $data_koperasi->groupBy(function ($item) {
+            return \Carbon\Carbon::parse($item->tanggal_data_koperasi)->translatedFormat('d F Y');
+        });
+    @endphp
+
     @foreach ($grouped as $tanggal => $data)
         @php
             $totalMasuk  = $data->where('jenis_data_koperasi', 'modal_masuk')
