005d60d350fb3dd6652c84e2f98dea4387290181
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-14 22:21:54 +0700

    update

--- resources/views/owner/data_koperasi/index_data_koperasi.blade.php
+++ resources/views/owner/data_koperasi/index_data_koperasi.blade.php
@@ -57,48 +57,21 @@
                                         </div>
                                         <div class="col-md-4">
                                             <div class="input-icon">
-                                                <select name="pilih_minggu" id="pilih_minggu" class="form-select">
-                                                    <option value="">Minggu Ke -</option>
-                                                    <option value="1">Minggu Ke - 1</option>
-                                                    <option value="2">Minggu Ke - 2</option>
-                                                    <option value="3">Minggu Ke - 3</option>
-                                                    <option value="4">Minggu Ke - 4</option>
-                                                </select>
+                                                <span class="input-icon-addon">
+                                                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M16 3l0 4" /><path d="M8 3l0 4" /><path d="M4 11l16 0" /><path d="M8 15h2v2h-2z" /></svg>
+                                                </span>
+                                                <input type="text" value="" id="dari_tanggal" name="dari_tanggal" class="form-control" placeholder="Dari Tanggal" autocomplete="off">
                                             </div>
                                         </div>
                                         <div class="col-md-4">
                                             <div class="input-icon">
-                                                <select name="bulan" id="bulan" class="form-select">
-                                                    <option value="">Pilih Bulan</option>
-                                                    <option value="Januari">Januari</option>
-                                                    <option value="Februari">Februari</option>
-                                                    <option value="Maret">Maret</option>
-                                                    <option value="April">April</option>
-                                                    <option value="Mei">Mei</option>
-                                                    <option value="Juni">Juni</option>
-                                                    <option value="Juli">Juli</option>
-                                                    <option value="Agustus">Agustus</option>
-                                                    <option value="September">September</option>
-                                                    <option value="Oktober">Oktober</option>
-                                                    <option value="November">November</option>
-                                                    <option value="Desember">Desember</option>
-                                                </select>
+                                                <span class="input-icon-addon">
+                                                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M16 3l0 4" /><path d="M8 3l0 4" /><path d="M4 11l16 0" /><path d="M8 15h2v2h-2z" /></svg>
+                                                </span>
+                                                <input type="text" value="" id="sampai_tanggal" name="sampai_tanggal" class="form-control" placeholder="Sampai Tanggal" autocomplete="off">
                                             </div>
                                         </div>
-                                        <div class="col-md-4">
-                                            <div class="input-icon">
-                                                <select name="pilih_tahun" id="pilih_tahun" class="form-select">
-                                                    <option value="">Pilih Tahun</option>
-                                                    <option value="2025">Tahun 2025</option>
-                                                    <option value="2026">Tahun 2026</option>
-                                                    <option value="2027">Tahun 2027</option>
-                                                    <option value="2028">Tahun 2028</option>
-                                                    <option value="2029">Tahun 2029</option>
-                                                    <option value="2030">Tahun 2030</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4">
+                                        <div class="col-md-6">
                                             <div class="form-group">
                                                 <button type="submit" class="btn btn-primary w-100">
                                                     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
@@ -107,7 +80,7 @@
                                             </div>
                                         </div>
                                 </form>
-                                        <div class="col-md-4">
+                                        <div class="col-md-6">
                                             <div class="form-group">
                                                 <a href="#" class="btn btn-success w-100" id="cetak_data_koperasi" >
                                                     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-printer"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" /><path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" /><path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" /></svg>
