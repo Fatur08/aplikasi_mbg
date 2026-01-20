8aa700ab49a148d401c23115f9fa669c6a42293b
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-21 00:52:40 +0700

    update

--- resources/views/admin/laporan/dapur/index_dapur.blade.php
+++ resources/views/admin/laporan/dapur/index_dapur.blade.php
@@ -207,39 +207,41 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <div class="form-group">
-                                                <a href="#" class="btn btn-success" id="btnTambahPM">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
-                                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" 
-                                                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-                                                         class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                                        <path d="M12 5l0 14" />
-                                                        <path d="M5 12l14 0" />
-                                                    </svg>
-                                                    Tambah P.Manfaat
-                                                </a>
-                                            </div>
-                                        </div>
-                                        <div class="col-6">
-                                            <div class="form-group">
-                                                <a href="#" class="btn btn-success" id="btnTambahOPDapur">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
-                                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" 
-                                                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-                                                         class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                                        <path d="M12 5l0 14" />
-                                                        <path d="M5 12l14 0" />
-                                                    </svg>
-                                                    Tambah Op Dapur
-                                                </a>
-                                            </div>
+                                </form>
+                            </div>
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <a href="#" class="btn btn-success" id="btnTambahPM">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" 
+                                                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                                                     class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                    <path d="M12 5l0 14" />
+                                                    <path d="M5 12l14 0" />
+                                                </svg>
+                                                Tambah P.Manfaat
+                                            </a>
                                         </div>
                                     </div>
-                                </form>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <a href="#" class="btn btn-success" id="btnTambahOPDapur">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" 
+                                                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                                                     class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                    <path d="M12 5l0 14" />
+                                                    <path d="M5 12l14 0" />
+                                                </svg>
+                                                Tambah Op Dapur
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="row mt-2 table-container">
