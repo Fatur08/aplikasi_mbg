899a6a1779d69200fedc344521e9d5c83f320476
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-20 21:34:24 +0700

    update

--- resources/views/admin/data_induk/index_data_induk.blade.php
+++ resources/views/admin/data_induk/index_data_induk.blade.php
@@ -263,6 +263,17 @@
                                 <div class="section-info">
                                     <div class="info-card">
                                         <h2><span style="color:#2563eb;">MAKER</span></h2>
+                                        <a href="#" class="btn btn-primary" id="btnTambahDapur">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                                                 viewBox="0 0 24 24" fill="none" stroke="currentColor" 
+                                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                                                 class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                <path d="M12 5l0 14" />
+                                                <path d="M5 12l14 0" />
+                                            </svg>
+                                            Tambah Data Maker
+                                        </a>
                                     </div>
                                 </div>
                                 <div class="d-flex justify-content-between align-items-center mb-3">
