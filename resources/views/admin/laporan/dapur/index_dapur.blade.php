37490be13caf68422077fa8bb1364528018c5f81
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-21 00:53:44 +0700

    update

--- resources/views/admin/laporan/dapur/index_dapur.blade.php
+++ resources/views/admin/laporan/dapur/index_dapur.blade.php
@@ -209,6 +209,8 @@
                                     </div>
                                 </form>
                             </div>
+                        </div>
+                        <div class="row mt-2">
                             <div class="col-12">
                                 <div class="row">
                                     <div class="col-6">
