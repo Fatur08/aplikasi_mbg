c4bb9d4dee3ef6c2dbb5fa169c39526785a1f2e0
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-22 01:13:28 +0700

    update

--- app/Http/Controllers/DataRelawanController.php
+++ app/Http/Controllers/DataRelawanController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class DataRelawanController extends Controller
 {
-    //
+    public function index_admin_data_staff_relawan(Request $request)
+    {
+        return view('admin.data_staff.relawan.index_relawan');
+    }
 }
