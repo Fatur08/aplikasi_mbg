97ec7f09061dabb5b022a0cf4c6c1e2ef9245bb4
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-22 00:54:22 +0700

    update

--- resources/views/admin/data_staff/maker/index_maker.blade.php
+++ resources/views/admin/data_staff/maker/index_maker.blade.php
@@ -150,15 +150,8 @@
                                 </a>
                             </td>
                             <td style="text-align:right">
-                                <a href="{{ url('/admin/data_staff') }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
-                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" 
-                                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-                                         class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                        <path d="M12 5l0 14" />
-                                        <path d="M5 12l14 0" />
-                                    </svg>
+                                <a href="{{ url('/admin/data_staff') }}" class="btn btn-primary">
+                                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg>
                                     Tambah Data
                                 </a>
                             </td>
