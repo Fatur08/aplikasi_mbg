83de3c703e815fab437a0d74fe5582048e9939c7
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-21 22:55:40 +0700

    update

--- resources/views/admin/laporan/keuangan/index_laporan_keuangan.blade.php
+++ resources/views/admin/laporan/keuangan/index_laporan_keuangan.blade.php
@@ -136,6 +136,19 @@
                                     Laporan Keuangan
                                 </h2>
                             </td>
+                            <td style="text-align:right">
+                                <a href="#" class="btn btn-primary" id="TambahLaporanKeuangan">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" 
+                                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                                         class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                        <path d="M12 5l0 14" />
+                                        <path d="M5 12l14 0" />
+                                    </svg>
+                                    Tambah Menu
+                                </a>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
@@ -388,7 +401,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="/owner/laporan/keuangan/store_laporan_keuangan" method="POST" id="frmLprnKngn" enctype="multipart/form-data">
+                <form action="/admin/laporan/keuangan/store_laporan_keuangan" method="POST" id="FormLaporanKeuangan" enctype="multipart/form-data">
                     @csrf
                     <div class="row">
                         <div class="col-12">
@@ -403,38 +416,10 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="input-icon mb-3">
-                                <select name="jenis_laporan_keuangan" id="jenis_laporan_keuangan" class="form-select">
-                                    <option value="">Pilih Jenis Transaksi</option>
-                                    <option value="Pemasukan">Pemasukan</option>
-                                    <option value="Pengeluaran">Pengeluaran</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="input-icon mb-3">
                                 <span class="input-icon-addon">
                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-category"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4h6v6h-6z" /><path d="M14 4h6v6h-6z" /><path d="M4 14h6v6h-6z" /><path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /></svg>
                                 </span>
-                                <input type="text" value="" id="kategori_laporan_keuangan" class="form-control" name="kategori_laporan_keuangan" placeholder="Masukkan Sumber/Tujuan">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="input-icon mb-3">
-                                <span class="input-icon-addon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" 
-                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-                                        class="icon icon-tabler icon-tabler-note">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                        <path d="M13 20h-7a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h8l6 6v8a2 2 0 0 1 -2 2h-3" />
-                                        <path d="M13 4v6h6" />
-                                    </svg>
-                                </span>
-                                <textarea id="keterangan_laporan_keuangan" name="keterangan_laporan_keuangan" class="form-control" rows="1" placeholder="Tuliskan keterangan di sini..."></textarea>
+                                <input type="text" value="" id="barang_laporan_keuangan" name="barang_laporan_keuangan" class="form-control"  placeholder="Masukkan Nama Barang">
                             </div>
                         </div>
                     </div>
@@ -444,7 +429,17 @@
                                 <span class="input-icon-addon">
                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-cashapp"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17.1 8.648a.568 .568 0 0 1 -.761 .011a5.682 5.682 0 0 0 -3.659 -1.34c-1.102 0 -2.205 .363 -2.205 1.374c0 1.023 1.182 1.364 2.546 1.875c2.386 .796 4.363 1.796 4.363 4.137c0 2.545 -1.977 4.295 -5.204 4.488l-.295 1.364a.557 .557 0 0 1 -.546 .443h-2.034l-.102 -.011a.568 .568 0 0 1 -.432 -.67l.318 -1.444a7.432 7.432 0 0 1 -3.273 -1.784v-.011a.545 .545 0 0 1 0 -.773l1.137 -1.102c.214 -.2 .547 -.2 .761 0a5.495 5.495 0 0 0 3.852 1.5c1.478 0 2.466 -.625 2.466 -1.614c0 -.989 -1 -1.25 -2.886 -1.954c-2 -.716 -3.898 -1.728 -3.898 -4.091c0 -2.75 2.284 -4.091 4.989 -4.216l.284 -1.398a.545 .545 0 0 1 .545 -.432h2.023l.114 .012a.544 .544 0 0 1 .42 .647l-.307 1.557a8.528 8.528 0 0 1 2.818 1.58l.023 .022c.216 .228 .216 .569 0 .773l-1.057 1.057z" /></svg>
                                 </span>
-                                <input type="number" value="" id="jumlah_dana" class="form-control" name="jumlah_dana" placeholder="Masukkan Jumlah Dana">
+                                <input type="number" value="" id="jumlah_barang_laporan_keuangan" name="jumlah_barang_laporan_keuangan" class="form-control" placeholder="Masukkan Jumlah Barang">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="input-icon mb-3">
+                                <span class="input-icon-addon">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-cashapp"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17.1 8.648a.568 .568 0 0 1 -.761 .011a5.682 5.682 0 0 0 -3.659 -1.34c-1.102 0 -2.205 .363 -2.205 1.374c0 1.023 1.182 1.364 2.546 1.875c2.386 .796 4.363 1.796 4.363 4.137c0 2.545 -1.977 4.295 -5.204 4.488l-.295 1.364a.557 .557 0 0 1 -.546 .443h-2.034l-.102 -.011a.568 .568 0 0 1 -.432 -.67l.318 -1.444a7.432 7.432 0 0 1 -3.273 -1.784v-.011a.545 .545 0 0 1 0 -.773l1.137 -1.102c.214 -.2 .547 -.2 .761 0a5.495 5.495 0 0 0 3.852 1.5c1.478 0 2.466 -.625 2.466 -1.614c0 -.989 -1 -1.25 -2.886 -1.954c-2 -.716 -3.898 -1.728 -3.898 -4.091c0 -2.75 2.284 -4.091 4.989 -4.216l.284 -1.398a.545 .545 0 0 1 .545 -.432h2.023l.114 .012a.544 .544 0 0 1 .42 .647l-.307 1.557a8.528 8.528 0 0 1 2.818 1.58l.023 .022c.216 .228 .216 .569 0 .773l-1.057 1.057z" /></svg>
+                                </span>
+                                <input type="number" value="" id="harga_barang_laporan_keuangan" name="harga_barang_laporan_keuangan" class="form-control" placeholder="Masukkan Total Harga Barang">
                             </div>
                         </div>
                     </div>
@@ -483,7 +478,7 @@
 @push('myscript')
 <script>
     $(function(){
-        $("#btnTambahLaporanKeuangan").click(function(){
+        $("#TambahLaporanKeuangan").click(function(){
             $("#modal-inputlaporankeuangan").modal("show");
         });
 
@@ -599,6 +594,15 @@
         });
         
         flatpickr("#sampai_tanggal", {
+            altInput: true,
+            altFormat: "d F Y",
+            dateFormat: "Y-m-d",
+            locale: "id",
+            allowInput: true
+        });
+
+
+        flatpickr("#tanggal_laporan_keuangan", {
             altInput: true,
             altFormat: "d F Y",
             dateFormat: "Y-m-d",
