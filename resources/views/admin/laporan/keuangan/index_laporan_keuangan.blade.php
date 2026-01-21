e5d1e8e63360250e47506e55116e192e2dce728f
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-21 22:56:33 +0700

    update

--- resources/views/admin/laporan/keuangan/index_laporan_keuangan.blade.php
+++ resources/views/admin/laporan/keuangan/index_laporan_keuangan.blade.php
@@ -146,7 +146,7 @@
                                         <path d="M12 5l0 14" />
                                         <path d="M5 12l14 0" />
                                     </svg>
-                                    Tambah Menu
+                                    Tambah Data
                                 </a>
                             </td>
                         </tr>
