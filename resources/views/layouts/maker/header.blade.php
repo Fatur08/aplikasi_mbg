6483855f6d36f1ce20e8cf73a49520b0fa4f13da
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-22 20:04:01 +0700

    update

--- resources/views/layouts/maker/header.blade.php
+++ resources/views/layouts/maker/header.blade.php
@@ -1,17 +1,17 @@
 @php
 use Illuminate\Support\Facades\DB;
 
-// Ambil user admin yang sedang login
-$admin = Auth::guard('admin')->user();
+// Ambil user maker yang sedang login
+$maker = Auth::guard('maker')->user();
 
 // Cek foto, tampilkan default jika kosong
-$path = $admin->foto_admin
-    ? asset('storage/uploads/data_induk/admin/' . $admin->foto_admin)
+$path = $maker->foto_maker
+    ? asset('storage/uploads/data_induk/maker/' . $maker->foto_maker)
     : asset('assets/img/nophoto.jpg');
 
-// Cocokkan nomor_dapur_admin dengan nomor_dapur di tabel dapur
+// Cocokkan nomor_dapur_maker dengan nomor_dapur di tabel dapur
 $nama_dapur = DB::table('dapur')
-    ->where('nomor_dapur', $admin->nomor_dapur_admin)
+    ->where('nomor_dapur', $maker->nomor_dapur_maker)
     ->value('nama_dapur');
 @endphp
 
@@ -22,12 +22,12 @@ $nama_dapur = DB::table('dapur')
                 <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
                     <span class="avatar avatar-sm" style="background-image: url('{{ $path }}')"></span>
                     <div class="d-none d-xl-block ps-2">
-                        <div>{{ $admin->nama_admin }}</div>
+                        <div>{{ $maker->nama_maker }}</div>
                         <div class="mt-1 small text-secondary">Maker ({{ $nama_dapur ?? 'Tidak Ada Dapur' }})</div>
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <a href="/proseslogoutadmin" class="dropdown-item">Logout</a>
+                    <a href="/proseslogoutmaker" class="dropdown-item">Logout</a>
                 </div>
             </div>
         </div>
