12e8044de87c2c23976c74c07808afa4f20eb666
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-23 13:43:43 +0700

    update

--- resources/views/maker/data_staff/index_data_staff.blade.php
+++ resources/views/maker/data_staff/index_data_staff.blade.php
@@ -792,7 +792,7 @@
                             <div class="col-12">
                                 <div class="section-info">
                                     <div class="info-card">
-                                        <h2><span style="color:#2563eb;">DISTRIBUTOR</span></h2>
+                                        <h2><span style="color:#2563eb;">driver</span></h2>
                                     </div>
                                 </div>
                                 <div class="table-wrapper">
@@ -811,32 +811,32 @@
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                                @foreach ($distributor as $d)
+                                                @foreach ($driver as $d)
                                                 @php
-                                                    $path = Storage::url('uploads/data_induk/distributor/'.$d->foto_distributor);
+                                                    $path = Storage::url('uploads/data_induk/driver/'.$d->foto_driver);
                                                 @endphp
                                                 <tr>
                                                     <td>{{ $loop->iteration }}</td>
-                                                    <td>{{ $d->nama_distributor }}</td>
-                                                    <td>{{ $d->email_distributor }}</td>
-                                                    <td>{{ $d->alamat_distributor }}</td>
-                                                    <td>{{ $d->no_hp_distributor }}</td>
+                                                    <td>{{ $d->nama_driver }}</td>
+                                                    <td>{{ $d->email_driver }}</td>
+                                                    <td>{{ $d->alamat_driver }}</td>
+                                                    <td>{{ $d->no_hp_driver }}</td>
                                                     <td>
-                                                        @if (empty($d->foto_distributor))
+                                                        @if (empty($d->foto_driver))
                                                         <img src="{{ asset('assets/img/nophoto.jpg') }}" class="avatar" alt="">
                                                         @else
                                                         <img src="{{ url($path) }}" class="avatar" alt="">
                                                         @endif
                                                     </td>
-                                                    <td>{{ $d->password_distributor }}</td>
+                                                    <td>{{ $d->password_driver }}</td>
                                                     <td>
                                                         <div class="btn-group">
-                                                            <a href="#" class="edit_distributor btn btn-info btn-sm" id="{{ $d->id_distributor }}" >
+                                                            <a href="#" class="edit_driver btn btn-info btn-sm" id="{{ $d->id_driver }}" >
                                                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
                                                             </a>
-                                                            <form action="/maker/data_induk/distributor/{{ $d->id_distributor }}/delete_distributor" style="margin-left: 5px;" method="POST">
+                                                            <form action="/maker/data_induk/driver/{{ $d->id_driver }}/delete_driver" style="margin-left: 5px;" method="POST">
                                                                 @csrf
-                                                                <a class="btn btn-danger btn-sm delete-confirm-distributor" >
+                                                                <a class="btn btn-danger btn-sm delete-confirm-driver" >
                                                                     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16z" /><path d="M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z" /></svg>
                                                                 </a>
                                                             </form>
