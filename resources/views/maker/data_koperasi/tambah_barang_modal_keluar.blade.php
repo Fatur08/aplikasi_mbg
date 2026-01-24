7211c4830d14af50b0ed423acf8009865df22b0a
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-24 15:00:03 +0700

    update

--- resources/views/maker/data_koperasi/tambah_barang_modal_keluar.blade.php
+++ resources/views/maker/data_koperasi/tambah_barang_modal_keluar.blade.php
@@ -6,151 +6,16 @@
     <input type="text" readonly value="{{ $data->nomor_dapur_data_koperasi }}"        id="pilih_dapur_modal_keluar"   class="form-control"    name="pilih_dapur_modal_keluar"     hidden>
     <div class="row">
         <div class="col-12">
-            <h4>Masukkan Barang Ke-1</h4>
+            <label class="form-label">Jumlah Barang</label>
+            <select class="form-control" id="jumlah_barang">
+                <option value="">-- Pilih Jumlah Barang --</option>
+                @for($i = 1; $i <= 10; $i++)
+                    <option value="{{ $i }}">{{ $i }} Barang</option>
+                @endfor
+            </select>
         </div>
     </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                  <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-codesandbox"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 7.5v9l-4 2.25l-4 2.25l-4 -2.25l-4 -2.25v-9l4 -2.25l4 -2.25l4 2.25z" /><path d="M12 12l4 -2.25l4 -2.25" /><path d="M12 12l0 9" /><path d="M12 12l-4 -2.25l-4 -2.25" /><path d="M20 12l-4 2v4.75" /><path d="M4 12l4 2l0 4.75" /><path d="M8 5.25l4 2.25l4 -2.25" /></svg>
-                </span>
-                <input type="text" value="" id="nama_barang_modal_keluar_1" class="form-control" name="nama_barang_modal_keluar_1" placeholder="Masukkan Nama Barang Modal Keluar Ke=1">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calculator"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 3m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 7m0 1a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-6a1 1 0 0 1 -1 -1z" /><path d="M8 14l0 .01" /><path d="M12 14l0 .01" /><path d="M16 14l0 .01" /><path d="M8 17l0 .01" /><path d="M12 17l0 .01" /><path d="M16 17l0 .01" /></svg>
-                </span>
-                <input type="number" value="" id="jumlah_barang_modal_keluar_1" name="jumlah_barang_modal_keluar_1" class="form-control" placeholder="Masukkan Jumlah Barang Modal Keluar Ke-1" autocomplete="off">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                  <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-number-1-small"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11 8h1v8" /></svg>
-                </span>
-                <input type="text" value="" id="satuan_barang_modal_keluar_1" class="form-control" name="satuan_barang_modal_keluar_1" placeholder="Masukkan Satuan Barang Modal Keluar Ke-1 (Contoh : Box, Dus, Gram, Dll)">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                  <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-coin"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 1 0 0 4h2a2 2 0 1 1 0 4h-2a2 2 0 0 1 -1.8 -1" /><path d="M12 7v10" /></svg>
-                </span>
-                <input type="number" value="" id="harga_barang_modal_keluar_1" class="form-control" name="harga_barang_modal_keluar_1" placeholder="Masukkan Harga Barang Modal Keluar Ke-1">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <h4>Masukkan Barang Ke-2 (Opsional)</h4>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                  <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-codesandbox"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 7.5v9l-4 2.25l-4 2.25l-4 -2.25l-4 -2.25v-9l4 -2.25l4 -2.25l4 2.25z" /><path d="M12 12l4 -2.25l4 -2.25" /><path d="M12 12l0 9" /><path d="M12 12l-4 -2.25l-4 -2.25" /><path d="M20 12l-4 2v4.75" /><path d="M4 12l4 2l0 4.75" /><path d="M8 5.25l4 2.25l4 -2.25" /></svg>
-                </span>
-                <input type="text" value="" id="nama_barang_modal_keluar_2" class="form-control" name="nama_barang_modal_keluar_2" placeholder="Masukkan Nama Barang Modal Keluar Ke=2">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calculator"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 3m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 7m0 1a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-6a1 1 0 0 1 -1 -1z" /><path d="M8 14l0 .01" /><path d="M12 14l0 .01" /><path d="M16 14l0 .01" /><path d="M8 17l0 .01" /><path d="M12 17l0 .01" /><path d="M16 17l0 .01" /></svg>
-                </span>
-                <input type="number" value="" id="jumlah_barang_modal_keluar_2" name="jumlah_barang_modal_keluar_2" class="form-control" placeholder="Masukkan Jumlah Barang Modal Keluar Ke-2" autocomplete="off">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                  <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-number-1-small"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11 8h1v8" /></svg>
-                </span>
-                <input type="text" value="" id="satuan_barang_modal_keluar_2" class="form-control" name="satuan_barang_modal_keluar_2" placeholder="Masukkan Satuan Barang Modal Keluar Ke-2 (Contoh : Box, Dus, Gram, Dll)">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                  <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-coin"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 1 0 0 4h2a2 2 0 1 1 0 4h-2a2 2 0 0 1 -1.8 -1" /><path d="M12 7v10" /></svg>
-                </span>
-                <input type="number" value="" id="harga_barang_modal_keluar_2" class="form-control" name="harga_barang_modal_keluar_2" placeholder="Masukkan Harga Barang Modal Keluar Ke-2">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <h4>Masukkan Barang Ke-3 (Opsional)</h4>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                  <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-codesandbox"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 7.5v9l-4 2.25l-4 2.25l-4 -2.25l-4 -2.25v-9l4 -2.25l4 -2.25l4 2.25z" /><path d="M12 12l4 -2.25l4 -2.25" /><path d="M12 12l0 9" /><path d="M12 12l-4 -2.25l-4 -2.25" /><path d="M20 12l-4 2v4.75" /><path d="M4 12l4 2l0 4.75" /><path d="M8 5.25l4 2.25l4 -2.25" /></svg>
-                </span>
-                <input type="text" value="" id="nama_barang_modal_keluar_3" class="form-control" name="nama_barang_modal_keluar_3" placeholder="Masukkan Nama Barang Modal Keluar Ke=3">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calculator"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 3m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 7m0 1a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-6a1 1 0 0 1 -1 -1z" /><path d="M8 14l0 .01" /><path d="M12 14l0 .01" /><path d="M16 14l0 .01" /><path d="M8 17l0 .01" /><path d="M12 17l0 .01" /><path d="M16 17l0 .01" /></svg>
-                </span>
-                <input type="number" value="" id="jumlah_barang_modal_keluar_3" name="jumlah_barang_modal_keluar_3" class="form-control" placeholder="Masukkan Jumlah Barang Modal Keluar Ke-3" autocomplete="off">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                  <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-number-1-small"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11 8h1v8" /></svg>
-                </span>
-                <input type="text" value="" id="satuan_barang_modal_keluar_3" class="form-control" name="satuan_barang_modal_keluar_3" placeholder="Masukkan Satuan Barang Modal Keluar Ke-3 (Contoh : Box, Dus, Gram, Dll)">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                  <!-- Download SVG icon from http://tabler-icons.io/i/user -->
-                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-coin"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 1 0 0 4h2a2 2 0 1 1 0 4h-2a2 2 0 0 1 -1.8 -1" /><path d="M12 7v10" /></svg>
-                </span>
-                <input type="number" value="" id="harga_barang_modal_keluar_3" class="form-control" name="harga_barang_modal_keluar_3" placeholder="Masukkan Harga Barang Modal Keluar Ke-3">
-            </div>
-        </div>
-    </div>
+    <div id="container-barang"></div>
     <div class="row mt-2">
         <div class="col-12">
             <div class="form-group">
@@ -165,71 +30,53 @@
 @endsection
 @push('myscript')
 <script>
-    $(function(){
-        $(".flatpickr").datepicker({
-            format: "yyyy-mm-dd",
-            autoclose: true,
-            todayHighlight: true
-        })
+$(document).ready(function () {
 
-        $("#frmTmbhBrgMdlKlr").submit(function(){
-            var pilih_dapur_modal_keluar = $("#pilih_dapur_modal_keluar").val();
-            var nama_barang_modal_keluar_1 = $("#nama_barang_modal_keluar_1").val();
-            var jumlah_barang_modal_keluar_1 = $("#jumlah_barang_modal_keluar_1").val();
-            var satuan_barang_modal_keluar_1 = $("#satuan_barang_modal_keluar_1").val();
-            var harga_barang_modal_keluar_1 = $("#harga_barang_modal_keluar_1").val();
-            if(pilih_dapur_modal_keluar==""){
-                Swal.fire({
-                    title: 'Warning!',
-                    text: 'Dapur Harus Diisi',
-                    icon: 'warning',
-                    confirmButtonText: 'OK'
-                  }).then(()=> {
-                      $("#pilih_dapur_modal_keluar").focus();
-                  });
-                return false;
-            } else if (nama_barang_modal_keluar_1==""){
-                Swal.fire({
-                    title: 'Warning!',
-                    text: 'Nama Barang Harus Diisi',
-                    icon: 'warning',
-                    confirmButtonText: 'OK'
-                  }).then(()=> {
-                      $("#nama_barang_modal_keluar_1").focus();
-                  });
-                return false;
-            } else if (jumlah_barang_modal_keluar_1==""){
-                Swal.fire({
-                    title: 'Warning!',
-                    text: 'Jumlah Barang Harus Diisi',
-                    icon: 'warning',
-                    confirmButtonText: 'OK'
-                  }).then(()=> {
-                      $("#jumlah_barang_modal_keluar_1").focus();
-                  });
-                return false;
-            } else if (satuan_barang_modal_keluar_1==""){
-                Swal.fire({
-                    title: 'Warning!',
-                    text: 'Satuan Barang Harus Diisi',
-                    icon: 'warning',
-                    confirmButtonText: 'OK'
-                  }).then(()=> {
-                      $("#satuan_barang_modal_keluar_1").focus();
-                  });
-                return false;
-            } else if (harga_barang_modal_keluar_1==""){
-                Swal.fire({
-                    title: 'Warning!',
-                    text: 'Harga Barang Harus Diisi',
-                    icon: 'warning',
-                    confirmButtonText: 'OK'
-                  }).then(()=> {
-                      $("#harga_barang_modal_keluar_1").focus();
-                  });
-                return false;
-            }
-        });
+    $('#jumlah_barang').on('change', function () {
+
+        let jumlah = $(this).val();
+        let html   = '';
+
+        if (jumlah === '') {
+            $('#container-barang').html('');
+            return;
+        }
+
+        for (let i = 1; i <= jumlah; i++) {
+
+            html += `
+            <div class="border rounded p-3 mb-3">
+                <h4>Masukkan Barang Ke-${i}</h4>
+
+                <div class="input-icon mb-3">
+                    <span class="input-icon-addon">📦</span>
+                    <input type="text" name="nama_barang_modal_keluar[]" class="form-control"
+                        placeholder="Nama Barang Ke-${i}" required>
+                </div>
+
+                <div class="input-icon mb-3">
+                    <span class="input-icon-addon">🔢</span>
+                    <input type="number" name="jumlah_barang_modal_keluar[]" class="form-control"
+                        placeholder="Jumlah Barang Ke-${i}" required>
+                </div>
+
+                <div class="input-icon mb-3">
+                    <span class="input-icon-addon">📐</span>
+                    <input type="text" name="satuan_barang_modal_keluar[]" class="form-control"
+                        placeholder="Satuan Barang Ke-${i}" required>
+                </div>
+
+                <div class="input-icon mb-3">
+                    <span class="input-icon-addon">💰</span>
+                    <input type="number" name="harga_barang_modal_keluar[]" class="form-control"
+                        placeholder="Harga Barang Ke-${i}" required>
+                </div>
+            </div>`;
+        }
+
+        $('#container-barang').html(html);
     });
+
+});
 </script>
 @endpush
