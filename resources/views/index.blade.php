c28358b8ec5b104a2cabce79f6bb384f62263197
Fatur08 <dhutafaturrahman@gmail.com>
2026-03-31 21:29:15 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Utama Login - Program MBG Lampung Timur</title>
+    <title>Halaman Utama Login gin gin - Program MBG Lampung Timur</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
