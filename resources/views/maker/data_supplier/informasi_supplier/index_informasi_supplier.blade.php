f4fae603b29655a607ab4875d834bc86ab51980b
Fatur08 <dhutafaturrahman@gmail.com>
2026-03-31 22:53:04 +0700

    update

--- resources/views/maker/data_supplier/informasi_supplier/index_informasi_supplier.blade.php
+++ resources/views/maker/data_supplier/informasi_supplier/index_informasi_supplier.blade.php
@@ -275,7 +275,16 @@
                                   <!-- Download SVG icon from http://tabler-icons.io/i/user -->
                                   <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
                                 </span>
-                                <input type="text" value="" id="nama_supplier" class="form-control" name="nama_supplier" placeholder="Masukkan Nama Lengkap">
+                                <input 
+                                    type="text"
+                                    id="nama_supplier"
+                                    name="nama_supplier"
+                                    class="form-control"
+                                    placeholder="Masukkan Nama Lengkap"
+                                    pattern="[A-Za-z\s]+"
+                                    title="Nama hanya boleh berisi huruf"
+                                    oninput="this.value = this.value.replace(/[^a-zA-Z\s]/g, '')"
+                                >
                             </div>
                         </div>
                     </div>
