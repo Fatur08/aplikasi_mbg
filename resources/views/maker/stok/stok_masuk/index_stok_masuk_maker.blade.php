a0b09370f653760d951894bce9a1463bad4d3334
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-02 11:33:21 +0700

    update

--- resources/views/maker/stok/stok_masuk/index_stok_masuk_maker.blade.php
+++ resources/views/maker/stok/stok_masuk/index_stok_masuk_maker.blade.php
@@ -434,7 +434,7 @@
                     </div>
                     <div class="row">
                         <div class="col-12 mb-3">
-                            <select name="id_bahan" id="id_bahan" class="form-select">
+                            <select name="nama_bahan" id="nama_bahan" class="form-select">
                                 <option value="">Pilih Bahan (Yang Tersedia Di Dapur)</option>
                                 @foreach($bahan as $item)
                                     <option value="{{ $item->nama_bahan }}">{{ $item->nama_bahan }}</option>
@@ -680,7 +680,7 @@
         var supplier_select = $("#supplier_select").val();
         var sumber_manual = $("#sumber_stok_masuk").val();
         var keterangan_stok_masuk = $("#keterangan_stok_masuk").val();
-        var id_bahan = $("#id_bahan").val();
+        var nama_bahan = $("#nama_bahan").val();
 
         if(tanggal_masuk==""){
             Swal.fire({
@@ -693,13 +693,13 @@
             return false;
         }
 
-        else if(id_bahan==""){
+        else if(nama_bahan==""){
             Swal.fire({
                 title: 'Warning!',
                 text: 'Bahan Harus Dipilih',
                 icon: 'warning'
             }).then(()=>{
-                $("#id_bahan").focus();
+                $("#nama_bahan").focus();
             });
             return false;
         }
