1fa02a8ffba489cc645ee10c1aad699462ecdc90
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-26 22:42:11 +0700

    update

--- resources/views/maker/operasional/informasi_operasional/index_informasi_operasional.blade.php
+++ resources/views/maker/operasional/informasi_operasional/index_informasi_operasional.blade.php
@@ -553,22 +553,6 @@
 
 
 
-            // BAGIAN EDIT HARGA OPERASIONAL
-            function hitungTotal() {
-                let jumlah = document.getElementById('edit_jumlah_jenis_informasi_operasional')?.value;
-                let harga = document.getElementById('edit_harga_satuan_informasi_operasional')?.value;
-
-                let total = (parseFloat(jumlah) || 0) * (parseFloat(harga) || 0);
-
-                let view = document.getElementById('edit_harga_informasi_operasional_view');
-                let hidden = document.getElementById('edit_harga_informasi_operasional');
-
-                if (view) view.value = total.toLocaleString('id-ID');
-                if (hidden) hidden.value = total;
-            }
-
-
-
 
 
 
@@ -650,5 +634,19 @@
                 allowInput: true
             });
         });
+
+        // BAGIAN EDIT HARGA OPERASIONAL
+        function hitungTotal() {
+            let jumlah = document.getElementById('edit_jumlah_jenis_informasi_operasional')?.value;
+            let harga = document.getElementById('edit_harga_satuan_informasi_operasional')?.value;
+
+            let total = (parseFloat(jumlah) || 0) * (parseFloat(harga) || 0);
+
+            let view = document.getElementById('edit_harga_informasi_operasional_view');
+            let hidden = document.getElementById('edit_harga_informasi_operasional');
+
+            if (view) view.value = total.toLocaleString('id-ID');
+            if (hidden) hidden.value = total;
+        }
     </script>
 @endpush
