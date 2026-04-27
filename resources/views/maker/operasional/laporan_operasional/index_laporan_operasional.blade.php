505a47a989cfecec2df9a239349ca8f422e64498
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-27 16:11:47 +0700

    update

--- resources/views/maker/operasional/laporan_operasional/index_laporan_operasional.blade.php
+++ resources/views/maker/operasional/laporan_operasional/index_laporan_operasional.blade.php
@@ -320,7 +320,7 @@
                                                                             <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
                                                                             <path
                                                                                 d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6
-                                                                                                                                                                                     c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                                                                                                                                                                                                 c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
                                                                         </svg>
                                                                         <span>Lihat</span>
                                                                     </a>
@@ -657,7 +657,7 @@
                         id: id
                     },
                     success: function (respond) {
-                        $("#loadeditinformasioperasional").html(respond);
+                        $("#load-edit-laporan-operasional").html(respond);
 
                         // 🔥 INIT DATEPICKER DI SINI
                         $("#edit_tanggal_laporan_operasional").flatpickr({
