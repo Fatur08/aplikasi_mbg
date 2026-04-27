da8b0360b229495137ad75e70190b17522e78c08
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-28 00:19:32 +0700

    update

--- resources/views/owner/operasional/informasi_operasional/index_informasi_operasional.blade.php
+++ resources/views/owner/operasional/informasi_operasional/index_informasi_operasional.blade.php
@@ -294,22 +294,10 @@
                                                                         @else
                                                                             <form
                                                                                 action="/owner/operasional/informasi_operasional/{{ $item->id_informasi_operasional }}/batalkan_validasi_informasi_operasional"
-                                                                                style="margin-left: 5px;" method="POST">
+                                                                                method="POST">
                                                                                 @csrf
                                                                                 <a
                                                                                     class="btn btn-sm bg-danger batalkan_validasi_informasi_operasional w-100">
-                                                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                                                        width="24" height="24" viewBox="0 0 24 24"
-                                                                                        fill="none" stroke="currentColor"
-                                                                                        stroke-width="2" stroke-linecap="round"
-                                                                                        stroke-linejoin="round"
-                                                                                        class="icon icon-tabler icons-tabler-outline icon-tabler-square-rounded-x">
-                                                                                        <path stroke="none" d="M0 0h24v24H0z"
-                                                                                            fill="none" />
-                                                                                        <path d="M10 10l4 4m0 -4l-4 4" />
-                                                                                        <path
-                                                                                            d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
-                                                                                    </svg>
                                                                                     Batalkan
                                                                                 </a>
                                                                             </form>
