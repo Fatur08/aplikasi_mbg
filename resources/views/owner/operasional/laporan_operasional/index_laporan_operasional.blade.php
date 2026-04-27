48321b17d0be7446e47043f7d8fc80ae88dc6d53
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-28 00:25:08 +0700

    update

--- resources/views/owner/operasional/laporan_operasional/index_laporan_operasional.blade.php
+++ resources/views/owner/operasional/laporan_operasional/index_laporan_operasional.blade.php
@@ -146,19 +146,6 @@
                                         Laporan Operasional
                                     </h2>
                                 </td>
-                                <td style="text-align:right">
-                                    <a href="#" class="btn btn-primary" id="btnTambahInformasiOperasional">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path d="M12 5l0 14" />
-                                            <path d="M5 12l14 0" />
-                                        </svg>
-                                        Tambah Data
-                                    </a>
-                                </td>
                             </tr>
                         </tbody>
                     </table>
@@ -320,7 +307,7 @@
                                                                             <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
                                                                             <path
                                                                                 d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6
-                                                                                                                                                                                                                                     c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                                                                                                                                                                                                                                                 c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
                                                                         </svg>
                                                                         <span>Lihat</span>
                                                                     </a>
