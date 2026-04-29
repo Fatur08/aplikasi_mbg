d358442245cc66bb3236c73a1fa5dbfdcc54375b
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-29 14:31:42 +0700

    update

--- resources/views/maker/data_koperasi/bukti_terima_data_koperasi.blade.php
+++ resources/views/maker/data_koperasi/bukti_terima_data_koperasi.blade.php
@@ -1,17 +1,23 @@
 <style>
     .bukti-terima-img {
-        max-width: 500px;   /* atur ukuran maksimal */
-        max-height: 450px;  /* biar tidak terlalu besar */
+        width: 100%;
+        /* isi penuh container */
+        max-width: 100%;
+        /* jangan lebih dari modal */
+        height: auto;
+        /* jaga rasio */
+        max-height: 80vh;
+        /* biar tidak keluar layar HP */
         object-fit: contain;
         display: block;
-        margin: auto;       /* center */
+        margin: auto;
     }
 </style>
 
 <div>
     @if(!empty($data->bukti_terima_data_koperasi))
-        <img src="{{ asset('storage/uploads/data_koperasi/bukti_terima/'.$data->bukti_terima_data_koperasi) }}"
-             class="bukti-terima-img">
+        <img src="{{ asset('storage/uploads/data_koperasi/bukti_terima/' . $data->bukti_terima_data_koperasi) }}"
+            class="bukti-terima-img">
     @else
         <p class="text-muted text-center">Tidak ada Bukti Terima</p>
     @endif
